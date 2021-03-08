b6e941209ed53ae1dc6642ab344ab766fadbe530
Arek Cichocki <arexspeed95@gmail.com>
2021-03-08 11:51:44 +0100

    api make & show

--- ncrash/pages/article/[id]/index.js
+++ ncrash/pages/article/[id]/index.js
@@ -1,42 +1,74 @@
+import { server } from '../../../config'
 import Link from 'next/link'
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
 
-const article = ({article}) => {
-  const router = useRouter()
-  const {id} = router.query
+const article = ({ article }) => {
+  // const router = useRouter()
+  // const { id } = router.query
+
   return (
     <>
-    <h1>{article.title}</h1>
+      <div/>
+      <h1>{article.title}</h1>
       <p>{article.body}</p>
       <br />
-      <Link href="/">Go Back</Link>
+      <Link href='/'>Go Back</Link>
     </>
   )
 }
 
 export const getStaticProps = async (context) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+  const res = await fetch(`${server}/api/articles/${context.params.id}`)
+
   const article = await res.json()
 
   return {
     props: {
-      article
-    }
+      article,
+    },
   }
 }
 
 export const getStaticPaths = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+  const res = await fetch(`${server}/api/articles`)
+
   const articles = await res.json()
 
-  const ids = articles.map(article => article.id)
-
-  const paths = ids.map(id => ({params: {id: id.toString()}}))
+  const ids = articles.map((article) => article.id)
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }))
 
   return {
     paths,
-    fallback: false
+    fallback: false,
   }
 }
+
+// export const getStaticProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   )
+
+//   const article = await res.json()
+
+//   return {
+//     props: {
+//       article,
+//     },
+//   }
+// }
+
+// export const getStaticPaths = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+//   const articles = await res.json()
+
+//   const ids = articles.map((article) => article.id)
+//   const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+
+//   return {
+//     paths,
+//     fallback: false,
+//   }
+// }
 
 export default article

0b3025ab6f7f6d0fefbacdeb37614fe604a881c3
Arek Cichocki <arexspeed95@gmail.com>
2021-04-09 14:23:44 +0200

    article services

--- fs_blog/pages/index.js
+++ fs_blog/pages/index.js
@@ -1,11 +1,11 @@
 import Layout from 'components/Layout'
 import Head from 'next/head'
-import {getList} from 'lib/markdownParser'
 import Image from 'next/image';
 import Link from 'next/link'
+import { getListOfArticles } from 'services/articles';
 
 export const getStaticProps = () => {
-  const articles = getList('_articles')
+  const articles = getListOfArticles('_articles')
 
   return {
     props: {articles}

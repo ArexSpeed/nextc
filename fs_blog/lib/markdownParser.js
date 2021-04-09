c59d70bcef0cd552b22f10f9b983ab9dec4ae8c0
Arek Cichocki <arexspeed95@gmail.com>
2021-04-09 14:17:03 +0200

    add & parser articles

--- fs_blog/lib/markdownParser.js
+++ fs_blog/lib/markdownParser.js
@@ -1,6 +1,9 @@
 import fs from 'fs'
 import {join} from 'path'
 import matter from 'gray-matter'
+import remark from 'remark';
+import html from 'remark-html';
+//import prism from 'remark-prism';
 
 export const getList = (path) => {
   const directory = join(process.cwd(), path);
@@ -10,7 +13,7 @@ export const getList = (path) => {
   return files.map((file) => {
     const fullPath = join(directory, file);
     const fileContents = fs.readFileSync(fullPath, 'utf8'); //czytanie pliku
-    const { data } = matter(fileContents); // zczytanie zawartowsci pliku i zapisnie do zmiennej
+    const { data } = matter(fileContents); // zczytanie zawartowsci pliku i zapisnie do zmiennej (title, desc etc)
 
     return {
       ...data,
@@ -18,4 +21,24 @@ export const getList = (path) => {
       createdAt: data.date ? Number(new Date(data.date)) : null
     };
   });
+};
+
+//pobranie pliku ze slug (dla konkrentego article)
+export const getFileBySlug = async (path, slug) => {
+  const directory = join(process.cwd(), path);
+  const fullPath = join(directory, `${slug}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+  const { data, content: markdownContent } = matter(fileContents);
+  let content = '';
+  if (markdownContent) {
+    content = await remark().use(html).process(markdownContent);
+    content = content.toString();
+  }
+
+  return {
+    ...data,
+    content,
+    slug,
+    createdAt: data.date ? Number(new Date(data.date)) : null
+  };
 };

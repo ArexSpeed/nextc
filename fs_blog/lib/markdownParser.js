78b599dd7f1026093123f4f2286d07a3378c0e2e
Arek Cichocki <arexspeed95@gmail.com>
2021-04-09 11:12:41 +0200

    parsing md

--- fs_blog/lib/markdownParser.js
+++ fs_blog/lib/markdownParser.js
@@ -1,11 +1,21 @@
 import fs from 'fs'
-import {join} from "path"
+import {join} from 'path'
+import matter from 'gray-matter'
 
 export const getList = (path) => {
   const directory = join(process.cwd(), path);
   // process.cwd - zwraca dobrą sciezke do plikow na kazdym pc
   const files = fs.readdirSync(directory);
 
-  return files;
-}
+  return files.map((file) => {
+    const fullPath = join(directory, file);
+    const fileContents = fs.readFileSync(fullPath, 'utf8'); //czytanie pliku
+    const { data } = matter(fileContents); // zczytanie zawartowsci pliku i zapisnie do zmiennej
 
+    return {
+      ...data,
+      slug: file.replace('.md', ''),
+      createdAt: data.date ? Number(new Date(data.date)) : null
+    };
+  });
+};

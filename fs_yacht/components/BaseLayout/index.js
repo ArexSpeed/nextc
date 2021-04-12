9b01d670ab16f81566fed69ea2052db5d9ee08e5
Arek Cichocki <arexspeed95@gmail.com>
2021-04-12 20:18:26 +0200

    admin panel

--- fs_yacht/components/BaseLayout/index.js
+++ fs_yacht/components/BaseLayout/index.js
@@ -49,7 +49,13 @@ const TopNavigation = () => {
             </a>
           </Link>
         )}
-
+        {session && session.user.role === 'admin' && (
+          <Link href="/admin/offers">
+            <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white">
+              <span>Admin</span>
+            </a>
+          </Link>
+        )}
         {session && (
           <button
             onClick={signOut}

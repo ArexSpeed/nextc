1cf47ed3b9d2e64368e8b042e60119a8b5d2320e
Arek Cichocki <arexspeed95@gmail.com>
2021-05-21 17:37:48 +0200

    basic crud

--- fs_match/pages/api/hello.js
+++ fs_match/pages/api/hello.js
@@ -1,5 +1,30 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { user } from 'models';
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' });
+export default async (req, res) => {
+  //example of create
+  /*
+  await user.create({
+    data: {
+      email: 'artur@email.com',
+      name: 'Arek',
+      image: 'test.jpg'
+    }
+  });
+  await user.update({
+    where: {
+      id: 3
+    },
+    data: {
+      name: 'Artur'
+    }
+  });
+  await user.delete({
+    where: {
+      id: 3
+    }
+  });
+  */
+  const allUsers = await user.findMany();
+
+  res.status(200).json({ allUsers });
 };

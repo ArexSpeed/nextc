6f012acb5e780e09c7eaca51336691cbed8865d2
Arek Cichocki <arexspeed95@gmail.com>
2021-05-23 10:45:14 +0200

    middleware

--- fs_match/pages/api/hello.js
+++ fs_match/pages/api/hello.js
@@ -1,30 +1,7 @@
-import { user } from 'models';
+import onlyAuth from 'middlewares/onlyAuth';
 
-export default async (req, res) => {
-  //example of create
-  /*
-  await user.create({
-    data: {
-      email: 'artur@email.com',
-      name: 'Arek',
-      image: 'test.jpg'
-    }
-  });
-  await user.update({
-    where: {
-      id: 3
-    },
-    data: {
-      name: 'Artur'
-    }
-  });
-  await user.delete({
-    where: {
-      id: 3
-    }
-  });
-  */
-  const allUsers = await user.findMany();
-
-  res.status(200).json({ allUsers });
+const helloApi = async (req, res) => {
+  res.status(200).json({ user: req.currentUser });
 };
+
+export default onlyAuth(helloApi);

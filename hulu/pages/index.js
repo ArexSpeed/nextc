85b2837362f39b9b7aab49bc13d2cba0204492fe
Arek Cichocki <arexspeed95@gmail.com>
2021-06-06 23:56:23 +0200

    header

--- hulu/pages/index.js
+++ hulu/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
 
       <h1>hulu</h1>
+      <Header />
     </div>
   )
 }

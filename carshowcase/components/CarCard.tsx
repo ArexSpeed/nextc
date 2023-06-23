6d48d03962691e82bf4740dc819f8da6cf619ea9
Arek <arexspeed95@gmail.com>
2023-06-23 13:01:29 +0200

    modal car detail

--- carshowcase/components/CarCard.tsx
+++ carshowcase/components/CarCard.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import React, { useState } from "react";
 import { calculateCarRent, generateCarImageUrl } from "@/utils";
 import { CustomButton } from "./CustomButton";
+import { CarDetails } from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
@@ -78,7 +79,11 @@ export const CarCard = ({ car }: CarCardProps) => {
         </div>
       </div>
 
-      {/* <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} /> */}
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   );
 };
--- carshowcase/utils/index.ts
+++ carshowcase/utils/index.ts
@@ -35,7 +35,7 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
 
   url.searchParams.append(
     "customer",
-    process.env.NEXT_PUBLIC_IMAGIN_API_KEY || ""
+    process.env.NEXT_PUBLIC_IMAGIN_API_KEY || "hrjavascript-mastery"
   );
   url.searchParams.append("make", make);
   url.searchParams.append("modelFamily", model.split(" ")[0]);

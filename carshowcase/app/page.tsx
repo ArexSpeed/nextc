83ad296d06b506bdd10afec049bb3c2c96ca6f63
Arek <arexspeed95@gmail.com>
2023-06-23 14:38:03 +0200

    search functionality

--- carshowcase/app/page.tsx
+++ carshowcase/app/page.tsx
@@ -3,10 +3,18 @@ import { CustomFilter } from "@/components/CustomFilter";
 import { Hero } from "@/components/Hero";
 import { SearchBar } from "@/components/SearchBar";
 import { fetchCars } from "@/utils";
+import { fuels, yearsOfProduction } from "@/constants";
 import Image from "next/image";
+import { HomeProps } from "@/types";
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   console.log({ allCars });
@@ -21,8 +29,8 @@ export default async function Home() {
         <div className="home_filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 

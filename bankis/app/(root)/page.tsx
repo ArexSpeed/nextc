7a96821b45c27d2bfda6db167596cd4a7d518451
Arek <arexspeed95@gmail.com>
2024-07-18 23:11:17 +0200

    add sidebars

--- bankis/app/(root)/page.tsx
+++ bankis/app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import { HeaderBox } from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import { TotalBalanceBox } from "@/components/TotalBalanceBox";
 import React from "react";
 
@@ -21,6 +22,8 @@ const Home = () => {
           totalCurrentBalance={1250.0}
         />
       </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
     </section>
   );
 };

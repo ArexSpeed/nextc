4aaf0f0c43ad6683972ec558b3a755185628dd68
Arek <arexspeed95@gmail.com>
2024-07-22 18:45:50 +0200

    implement plaid and dwolla

---
+++ bankis/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c7b5a24d007e5c72e57457e3ae57f164@o4507645972054016.ingest.de.sentry.io/4507645974806608",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});

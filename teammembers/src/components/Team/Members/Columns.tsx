13d79949445f5f1ff46f0e66ac3bae69495ee3cb
Arek <arexspeed95@gmail.com>
2024-11-12 20:50:43 +0100

    add options and roles column

--- teammembers/src/components/Team/Members/Columns.tsx
+++ teammembers/src/components/Team/Members/Columns.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 import { useHelpers } from "@/hooks/useHelpers";
 import { ColumnDef } from "@tanstack/react-table";
+import Roles from "./Options/Roles";
+import { Badge } from "@/components/ui/badge";
+import Options from "./Options";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const columns: ColumnDef<any>[] = [
   {
     accessorKey: "name",
     header: "Name",
     cell: ({ row }) => {
       const name: string = row.getValue("name");
-      const email: string = row.original.email;
+      // const email: string = row.original.email;
       return (
         <div className="flex items-center gap-2">
           <div className="flex items-center justify-center w-8 h-8 font-bold text-white capitalize bg-black rounded-full">
@@ -15,7 +21,7 @@ export const columns: ColumnDef<any>[] = [
           </div>
           <div className="grid">
             <span className="font-medium">{name}</span>
-            <span className="text-xs text-neutral-500">{email}</span>
+            {/* <span className="text-xs text-neutral-500">{email}</span> */}
           </div>
         </div>
       );
@@ -25,6 +31,7 @@ export const columns: ColumnDef<any>[] = [
     accessorKey: "role",
     header: "Role",
     cell: ({ row }) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
       const { open, setOpen, loading, setLoading } = useHelpers();
       const role: string = row.getValue("role");
 
@@ -46,6 +53,58 @@ export const columns: ColumnDef<any>[] = [
           {!open && (
             <span className="text-sm capitalize text-neutral-500">{role}</span>
           )}
+          {open && (
+            <Roles
+              {...{ selected: role }}
+              setSelected={(v) => onRoleChanged(v)}
+            />
+          )}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const status: string = row.getValue("status");
+
+      switch (status) {
+        case "pending":
+          return (
+            <Badge className="text-orange-900 capitalize hover:bg-transparent bg-orange-50">
+              Pending
+            </Badge>
+          );
+        case "active":
+          return (
+            <Badge className="text-green-900 capitalize hover:bg-transparent bg-green-50">
+              Active
+            </Badge>
+          );
+        case "removed":
+          return (
+            <Badge className="text-red-900 capitalize hover:bg-transparent bg-red-50">
+              Removed
+            </Badge>
+          );
+        default:
+          return (
+            <Badge className="capitalize hover:bg-transparent bg-neutral-50 text-neutral-900">
+              Unknown
+            </Badge>
+          );
+      }
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const user = row.original;
+
+      return (
+        <div className="flex justify-end">
+          <Options {...{ user }} />
         </div>
       );
     },
---
+++ teammembers/src/components/Team/Members/Options/index.tsx
@@ -0,0 +1,79 @@
+import {
+  Cloud,
+  CreditCard,
+  EllipsisVertical,
+  Github,
+  Keyboard,
+  LifeBuoy,
+  LogOut,
+  Mail,
+  MessageSquare,
+  Plus,
+  PlusCircle,
+  Settings,
+  User,
+  UserPlus,
+  Users,
+  UserX,
+} from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useHelpers } from "@/hooks/useHelpers";
+import Remove from "./Remove";
+
+export default function DropdownMenuDemo() {
+  const { open = false, setOpen, selected, setSelected } = useHelpers();
+
+  const menu: any = [
+    {
+      title: "Remove member",
+      key: "remove",
+      icon: <UserX className="w-[20px]" />,
+    },
+  ];
+  return (
+    <>
+      <Remove
+        {...{
+          user,
+          open: selected === "remove",
+          onClose: () => setSelected(undefined),
+        }}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <EllipsisVertical className="w-4 cursor-pointer" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuGroup>
+            {menu.map((item: any, i: number) => (
+              <div key={i}>
+                <DropdownMenuItem
+                  className="flex gap-2 cursor-pointer"
+                  onClick={() => setSelected(item.key)}
+                >
+                  {item.icon}
+                  <span>{item.title}</span>
+                </DropdownMenuItem>
+              </div>
+            ))}
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
